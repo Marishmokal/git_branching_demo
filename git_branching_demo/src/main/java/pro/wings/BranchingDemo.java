d59f5f9ed7f0819f1c722667b689c1ad3991d7b1
Marishmokal <113086159+Marishmokal@users.noreply.github.com>
2023-02-15 16:42:10 +0530

    Update BranchingDemo.java

--- git_branching_demo/src/main/java/pro/wings/BranchingDemo.java
+++ git_branching_demo/src/main/java/pro/wings/BranchingDemo.java
@@ -4,6 +4,7 @@ public class BranchingDemo {
 public static void main(String[] args) {
 	int i=2;
 	int j=12;
+//Done with addition part
 	int result=i+j;
 	System.out.println("Result is:-"+result);
 }
