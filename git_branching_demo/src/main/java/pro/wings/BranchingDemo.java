95c958a62a8da78fd664dd9da9d5b1b26149e7e5
Marishmokal <113086159+Marishmokal@users.noreply.github.com>
2023-02-15 18:55:46 +0530

    Dev5 commit

--- git_branching_demo/src/main/java/pro/wings/BranchingDemo.java
+++ git_branching_demo/src/main/java/pro/wings/BranchingDemo.java
@@ -5,13 +5,17 @@ public static void main(String[] args) {
 	int i=2;
 	int j=12;
 //Done with addition part
+	//dev4
 	//Tips to Become a Good Java Developer
 //10 Essential Tools for Java Developers
+	//dev4
 //Top Skills to Become a Full-Stack Java Developer
 	int result=i+j;
 	System.out.println("Result is:-"+result);
+	//dev4
 	
 	//Another method is multiplication
+	//dev4
 	//i m trying to do it
 }
 }
