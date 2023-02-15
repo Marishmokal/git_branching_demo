88d1e5d8705e334862c0631517982252c11f33b2
Marishmokal <marishmokal96@gmail.com>
2023-02-15 16:40:39 +0530

    first commit

---
+++ git_branching_demo/src/main/java/pro/wings/BranchingDemo.java
@@ -0,0 +1,10 @@
+package pro.wings;
+
+public class BranchingDemo {
+public static void main(String[] args) {
+	int i=2;
+	int j=12;
+	int result=i+j;
+	System.out.println("Result is:-"+result);
+}
+}
